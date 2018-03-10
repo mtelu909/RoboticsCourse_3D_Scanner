54d092cc3220ef016817b053ed76ea3908fcc94a
Enmar123 <js_enmar1@hotmail.com>
2018-03-10 13:35:16 -0500

    6th commit
    6th commit

--- 3D_Scan/Array_Scan.py
+++ 3D_Scan/Array_Scan.py
@@ -1,13 +1,21 @@
-#DESCRIPTION
-#Program to read a laserline and turn it into an array of heights
-#(Incomplete)
+# DESCRIPTION
+# Program to read a laserline and turn it into an array of heights
+# (Incomplete)
 
-#Initializing the Program
+# Initializing the Program
 import cv2
 import time
 import numpy as np
 
-#Setting up Functions
+# User Inputs ----------------------------------------------------------
+xcount = 48				# Number of slices
+gap = 20				# Pixel Width of slice 
+H = 540					# Pixel Height of Window
+W = 960					# Pixel Width of Window
+scale = 1				# Conversion pixels to inches (or mm)
+#-----------------------------------------------------------------------
+
+# Setting up Functions
 def centroid( mask ):
 	M = cv2.moments(mask,True)
 	m00 = M['m00']
@@ -32,45 +40,35 @@ def remember(points, point):
 	points.append((x, y))	
 	return points
 
-#Setting up Variables	
+# Setting up Variables	
 c = []
 xarr = []
 yarr = []
+zdist = []
 
-#Reading the Image
-img = cv2.imread('/home/jason/RoboticsCourse_3D_Scanner/3D_Scan/laserline.jpg')
+# Reading the Image
+img = cv2.imread('/home/enmar/RoboticsCourse_3D_Scanner/3D_Scan/laserline1.jpg')
 
-#Display Image
-imS = cv2.resize(img, (960, 540))		
-#cv2.imshow('image',imS) 				#Feedback: Resized image
+# Display Image
+imS = cv2.resize(img, (W, H))		
+#cv2.imshow('image',imS) 				# Feedback: Resized image
 
-#Masking With lines
-#Desc:	Draw a vertical line and give it a desired width. Results in
-#		a rectangular box that is filled in.
-cv2.line(imS, (100, 1), (100, 540), (0,0,0), 50)
-
-
-
-#cv2.imshow("line", imS)					#Feedback: Masking line
-
-xcount = 48
-gap = 20
 
 for i in range(0,xcount): 
 	
-	#Slicing Math
+	# Slicing Math
 	inc = i*gap
-	pos1 = -960 + inc
-	pos2 =  960 + gap + inc
-	width = 960*2
+	pos1 = -W + inc
+	pos2 =  W + gap + inc
+	width = W*2
 	
-	imS = cv2.resize(img, (960, 540))
-	cv2.line(imS, (pos1, 1), (pos1, 540), (0,0,0), width)
-	cv2.line(imS, (pos2, 1), (pos2, 540), (0,0,0), width)
+	imS = cv2.resize(img, (W, H))
+	cv2.line(imS, (pos1, 1), (pos1, H), (0,0,0), width)
+	cv2.line(imS, (pos2, 1), (pos2, H), (0,0,0), width)
 	
-	#cv2.imshow("Slice", imS)				#Feedback: Slicing Animation
+	#cv2.imshow("Slice", imS)			# Feedback: Slicing Animation
 	
-	#Masking Process
+	# Masking Process
 	hsv = cv2.cvtColor(imS, cv2.COLOR_BGR2HSV)
 	h = 225 * 176/255
 	s = 240
@@ -82,11 +80,11 @@ for i in range(0,xcount):
 		mask = cv2.erode(mask, None, iterations = 1)
 		mask = cv2.dilate(mask, None, iterations = 1)
 	
-	cv2.imshow('mask', mask)				#Feedback: Masking Animation
+	cv2.imshow('mask', mask)			# Feedback: Masking Animation
 	
 	x,y = centroid(mask)
 	c = remember(c, (x,y))
-	draw(imS, c, (28,172,244))				#Feedback: Drawing Animation
+	draw(imS, c, (28,172,244))			# Feedback: Drawing Animation
 	
 	xarr.append(x)
 	yarr.append(y)
@@ -96,17 +94,34 @@ for i in range(0,xcount):
 	if cv2.waitKey(1) > -1:
 		break
 
+# Processing heights (Z)
+yarr = np.array(yarr)
+zdist = H - yarr
+jump = zdist[0]
+zdist = zdist - jump
+
+for i in range(0,xcount):
+	if zdist[i] < 0:
+		zdist[i] = 0
+
+zdist = zdist * scale
+
+# Processing slice distances (X)
+	
+
+# Feedback
 print('c =', c)
 print()
 print('xarr = ', xarr)	
 print()
 print('yarr = ', yarr)
+print()
+print('zdist = ', zdist)
 
 
 
 
-
-#Hold Image untill Key-Press
+# Hold Image untill Key-Press
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
