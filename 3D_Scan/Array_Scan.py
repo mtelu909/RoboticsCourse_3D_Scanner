287de9e085ed77088b4b0372f0045c19de473210
Enmar123 <js_enmar1@hotmail.com>
2018-03-16 23:48:49 -0400

    Initial Pi-Capture Commit
    Code that should (emphasis on should) be able to run on the ras-pi and record an image every time a specified pin is triggered. This file has not been tested on a Ras-Pi yet so this statement is just a claim so far. The write-testfile.py contined within was used to test the capturing capabilities of the code using a webcam on a laptop.

--- 3D_Scan/Array_Scan.py
+++ 3D_Scan/Array_Scan.py
@@ -6,22 +6,26 @@
 import cv2
 import time
 import numpy as np
+import Rpi.GPIO as GPIO
+import os, shutil
 
-# User Inputs ----------------------------------------------------------
+# USER INPUTS ----------------------------------------------------------
+
+INPUT_PIN = 4           # Sets our input pin
+
 xcount = 48				# Number of slices
-scan_max = 5				# Number of pictures taken
+scan_max = 5			# Number of pictures taken
 gap = 20				# Pixel Width of slice 
 H = 540					# Pixel Height of Window
 W = 960					# Pixel Width of Window
 scale = 1				# Conversion pixels to inches (or mm)
 
-location = '/home/enmar/RoboticsCourse_3D_Scanner/3D_Scan/'
-basename = 'laserline'
+location = '/home/enmar/RoboticsCourse_3D_Scanner/3D_Scan/data'
+basename = 'pic'
 filetype = '.jpg'
 
-#-----------------------------------------------------------------------
+# Function Setup -------------------------------------------------
 
-# Setting up Functions
 def centroid( mask ):
 	M = cv2.moments(mask,True)
 	m00 = M['m00']
@@ -45,10 +49,56 @@ def remember(points, point):
 			return points
 	points.append((x, y))	
 	return points
+	
+def cleardata():
+	folder = location
+	for the_file in os.listdir(folder):
+		file_path = os.path.join(folder, the_file)
+		try:
+			if os.path.isfile(file_path):
+				os.unlink(file_path)
+			#elif os.path.isdir(file_path): shutil.rmtree(file_path)
+		except Exception as e:
+			print(e)
 
+# Capture Loop ---------------------------------------------------------
+ 
+GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
+GPIO.setup(INPUT_PIN, GPIO.IN)   # Set our input pin to be an input
 
+for scan in range(0,scan_max):
+	# Wait for button press
+	while true 
+		var = GPIO.input(INPUT_PIN)	
+		if ( var == True) && (var_old == False)
+			break	
+		var_old = var
+		time.sleep(.01)
+	
+	# take pic	
+	while True:
+		cap = cv2.VideoCapture(0)
+		okay, frame = cap.read()
+		cap.release()
+		if okay == False:
+			continue
+				
+		index = str(scan)
+		filename = location + basename + index + filetype
+		cv2.imwrite(filename, frame)
+		time.sleep(.1)
+		break
+	
+	# Feedback	
+	print(scan)
+	print(filename)
+		
+	
+			
+		
+		
 
-# MAIN LOOP-------------------------------------------------------------
+# Calculation Loop -----------------------------------------------------
 
 matrix = [[0 for x in range(xcount)] for y in range(scan_max)]
 
