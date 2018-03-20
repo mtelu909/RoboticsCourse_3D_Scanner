6eced114e9083898ab7d4c871dbf3987e6da4264
Enmar123 <36752813+Enmar123@users.noreply.github.com>
2018-03-20 00:56:50 -0400

    13th Commit - 3rd matlab plotting
    Improved the matlab plotting to incorporate Xmatrix and Ymatrix.

--- 3D_Scan/surfplot.m
+++ 3D_Scan/surfplot.m
@@ -9,10 +9,18 @@ clear all
 contour  = 30;      % # of isolines in plot
 
 % Plot --------------------------------------------------------------------
+figure(1)
+hold on
+Z_matrix = xlsread('Matrices.xlsx', 'Zmatrix')
+Y_matrix = xlsread('Matrices.xlsx', 'Ymatrix')
+X_matrix = xlsread('Matrices.xlsx', 'Xmatrix')
+surf(X_matrix, Y_matrix, Z_matrix)
 
-Z_matrix = xlsread('matrix.xlsx')
-surf(Z_matrix)
 shading interp
-%contour3(X,Y,Z,controur,'b')
+contour3(X_matrix, Y_matrix, Z_matrix, contour,'k')
 
+title('3D Scanner Plot')
+xlabel('X-axis')
+ylabel('Y-axis')
+zlabel('Z-axis')
 
